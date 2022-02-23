1f24b19dfb6df19ebfca87bb1eca7da1aefc23b0
mikicola <michela.colangelo@gmail.com>
2022-02-23 18:33:30 +0100

    aggiunta dei diversi bg-color

--- script.js
+++ script.js
@@ -6,25 +6,29 @@
 //     boxesArea.innerHTML += `<div class="box">${i}</div>`  
 // }
 
-let container = document.getElementById("boxes-area");
+let boxesArea = document.getElementById("boxes-area");
 
 
 for(let i = 1; i <= 100; i++){
 
     box = document.createElement("div");
     box.classList.add("box");
-    container.append(box);
+    boxesArea.append(box);
     
     if(i%3 == 0 && i%5 == 0){
         box.innerHTML = "FizzBuzz";
+        box.classList.add("color-one");
+
     }
-    
+
     else if(i%3 == 0){
         box.innerHTML = "Fizz";
+        box.classList.add("color-two");
     }
 
     else if(i%5 == 0){
         box.innerHTML = "Buzz";
+        box.classList.add("color-three");
     }
 
     else{
